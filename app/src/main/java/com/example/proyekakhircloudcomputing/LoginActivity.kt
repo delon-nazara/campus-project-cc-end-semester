3c9b99a49be2b414e6f19577875a5ce5969ee5e0
ferry <ferrypb123pb123@gmail.com>
2024-11-20 22:37:01 +0700

    1, 2

---
+++ app/src/main/java/com/example/proyekakhircloudcomputing/LoginActivity.kt
@@ -0,0 +1,112 @@
+package com.example.proyekakhircloudcomputing
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            LoginScreen(
+                onLoginClick = { _, _-> /* Tambahkan aksi tombol Masuk di sini */ },
+                onSignUpClick = {
+                    startActivity(Intent(this, SignUpActivity::class.java))
+                },
+                onBackClick = { finish() }
+            )
+        }
+    }
+}
+
+@Composable
+fun LoginScreen(onLoginClick: (String, String) -> Unit, onSignUpClick: () -> Unit, onBackClick: () -> Unit) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val backgroundColor = Color(0xFFF4F2DE)
+    Image(
+        painter = painterResource(id = R.drawable.title),
+        contentDescription = null,
+        modifier = Modifier.size(250.dp)
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp)
+
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.title),
+            contentDescription = null,
+            modifier = Modifier
+                .size(250.dp)
+                .align(Alignment.TopCenter)
+        )
+        Column(
+            modifier = Modifier
+                .shadow(3.dp, shape = RoundedCornerShape(16.dp))
+                .border(3.dp, Color.White, shape = RoundedCornerShape(16.dp))
+                .clip(RoundedCornerShape(16.dp))
+                .wrapContentWidth()
+                .align(alignment = Alignment.Center)
+                .background(Color(0xFFFf2a73b))
+                .padding(16.dp),
+        ) {
+            Text(text = "Masuk", fontSize = 25.sp, color = Color.Black, modifier = Modifier.align(Alignment.CenterHorizontally).padding(all = 16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Username") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { onLoginClick(username, password) },
+                colors = ButtonDefaults.buttonColors(Color(0xFFF4A9BB4)),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Masuk")
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(text = "Belum punya akun? Daftar sekarang", fontSize = 15.sp, modifier = Modifier.align(Alignment.CenterHorizontally))
+
+            Button(onClick = onSignUpClick, colors = ButtonDefaults.buttonColors(Color(0xFFF4A9BB4)), modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Daftar")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = onBackClick, colors = ButtonDefaults.buttonColors(Color(0xFFF4A9BB4)), modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Kembali")
+            }
+        }
+    }
+}
