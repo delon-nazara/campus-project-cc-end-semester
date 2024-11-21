6aeadf6516f0ab0be42fc558c7edc49f3e0cef31
StevenAnthony031 <steven.anthonyy92@gmail.com>
2024-11-21 23:26:57 +0700

    MainActivity (Home Screen Capsule) Selesai

--- app/src/main/java/com/example/proyekakhircloudcomputing/MainActivity.kt
+++ app/src/main/java/com/example/proyekakhircloudcomputing/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.proyekakhircloudcomputing
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -26,8 +27,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,25 +53,50 @@ fun MainScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFF2A73B))
+            .background(colorResource(R.color.blue_main))
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
         ) {
-            // Header di bagian atas
-            HeaderSection(showTexts = true)
-            Spacer(modifier = Modifier.height(16.dp))
+            // ===================================
+            // Pemanggil Function Header
+            // ===================================
+            HeaderSection()
 
-//             Konten utama (CapsulesSection)
+            Spacer(modifier = Modifier.height(35.dp))
+
+            Text(
+                text = "Halo, Arkan!",
+                fontSize = 25.sp,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                text = "Abadikan momen berharga mu!",
+                fontSize = 15.sp,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+
+            // ===================================
+            // Pemanggil Bagian Capsule
+            // ===================================
             CapsulesSection(
                 modifier = Modifier
-                    .weight(1f) // Mengisi ruang di antara Header dan Navbar
+                    .weight(1f)
                     .fillMaxSize()
             )
         }
 
-        // Navbar di bagian bawah layar
+        // ===================================
+        // Bottom Navigasi Bar
+        // ===================================
         Navbar(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
@@ -87,13 +118,21 @@ fun CapsulesSection(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(colorResource(R.color.yellow_background))
+            .background(
+                colorResource(R.color.yellow_background),
+                shape = RoundedCornerShape(topStart = 50.dp,topEnd = 50.dp),
+            )
     ) {
         Spacer(modifier = Modifier.height(32.dp))
         Box(
             modifier = Modifier
                 .padding(start = 16.dp)
                 .wrapContentSize()
+                .shadow(
+                    elevation = 8.dp,
+                    shape = RoundedCornerShape(50.dp),
+                    clip = false
+                )
                 .clip(RoundedCornerShape(50.dp))
                 .background(colorResource(R.color.orange))
                 .padding(horizontal = 24.dp, vertical = 16.dp)
@@ -101,15 +140,23 @@ fun CapsulesSection(modifier: Modifier = Modifier) {
             Text(
                 text = "Kapsul Public",
                 fontSize = 20.sp,
-                color = Color(0xFFF4F2DE)
+                color = colorResource(R.color.yellow_background),
+                fontWeight = FontWeight.SemiBold
             )
-
         }
+        // ===================================
+        // Pemanggil Lazy Row
+        // ===================================
         DynamicLazyRowWithButton()
         Box(
             modifier = Modifier
                 .padding(start = 16.dp)
                 .wrapContentSize()
+                .shadow(
+                    elevation = 8.dp, // Tinggi bayangan
+                    shape = RoundedCornerShape(50.dp), // Bentuk bayangan mengikuti RoundedCorner
+                    clip = false // Tidak memotong bayangan ke dalam batas shape
+                )
                 .clip(RoundedCornerShape(50.dp))
                 .background(colorResource(R.color.orange))
                 .padding(horizontal = 24.dp, vertical = 16.dp)
@@ -117,15 +164,20 @@ fun CapsulesSection(modifier: Modifier = Modifier) {
             Text(
                 text = "Kapsul Pribadi",
                 fontSize = 20.sp,
-                color = colorResource(R.color.yellow_background)
+                color = colorResource(R.color.yellow_background),
+                fontWeight = FontWeight.SemiBold
             )
         }
+        // ===================================
+        // Pemanggil Lazy Row
+        // ===================================
         DynamicLazyRowWithButton()
-
     }
 }
 
-
+// ===================================
+// Fungsi Lazy Row
+// ===================================
 @Composable
 fun DynamicLazyRowWithButton() {
     // State untuk melacak data dinamis di LazyRow
@@ -138,29 +190,45 @@ fun DynamicLazyRowWithButton() {
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        // Tombol "+" untuk menambahkan kapsul ke LazyRow
-        Box(
-            modifier = Modifier
-                .size(120.dp)
-                .clip(RoundedCornerShape(16.dp))
-                .background(colorResource(R.color.green_kahf))
-                .clickable {
-                    // Tambahkan kapsul baru ke dalam LazyRow
-                    capsules.add("Capsule ${capsules.size + 1}")
-                },
-            contentAlignment = Alignment.Center
-        ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Box(
+                modifier = Modifier
+                    .size(120.dp)
+                    .shadow(
+                        elevation = 8.dp,
+                        shape = RoundedCornerShape(50.dp),
+                        clip = false
+                    )
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(colorResource(R.color.green_kahf))
+                    .clickable {
+                        // Tambahkan kapsul baru ke dalam LazyRow
+                        capsules.add("Capsule ${capsules.size + 1}")
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+                // Tombol "+" untuk menambahkan kapsul ke LazyRow
+                Text(
+                    text = "+",
+                    fontSize = 50.sp,
+                    color = Color.White
+                )
+            }
             Text(
-                text = "+",
-                fontSize = 50.sp,
-                color = Color.White
+                modifier = Modifier.padding(8.dp),
+                text = "Buat Kapsul Baru",
+                fontSize = 16.sp,
+                color = Color.Black
             )
         }
-
+        //=======================================
         // LazyRow yang menampilkan kapsul
+        //=======================================
         LazyRow(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.weight(1f) // LazyRow mengambil sisa ruang
+            modifier = Modifier.weight(1f)
         ) {
             items(capsules.size) { index ->
                 CapsuleItem(title = capsules[index])
@@ -169,6 +237,9 @@ fun DynamicLazyRowWithButton() {
     }
 }
 
+// ===================================
+// Item Capsule
+// ===================================
 @Composable
 fun CapsuleItem(title: String) {
     Column(
@@ -183,16 +254,22 @@ fun CapsuleItem(title: String) {
                 .padding(16.dp),
             contentAlignment = Alignment.Center
         ) {
-            // Konten kapsul (contoh: judul)
-            Text(
-                text = title,
-                fontSize = 16.sp,
-                color = Color.White
+            // Konten kapsul (contoh: dummy)
+            Image(
+                painter = painterResource(id = R.drawable.pp),
+                contentDescription = "Image Capsule",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
             )
         }
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = title,
+            fontSize = 16.sp,
+            color = Color.Black
+        )
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
